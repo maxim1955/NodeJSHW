c129c2d8f992e93714e51ee04b404d9d459a931f
maxim1955 <nikitin.maxim.99@gmail.com>
2023-06-13 11:47:21 +0300

    test req

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-let a = 10
-let b = 20
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const getPost = (async () => {
+    const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    fs.writeFile(path.resolve(__dirname, '1.json'), JSON.stringify(data), 'utf8', (err) => {
+        if (err) {
+            throw err;
+        }
+    })
+    console.log("done")
+    return data;
+})
+http
+    .createServer((req, res) => {
+        res.setHeader('Content-Type', 'text/html;  charset=utf-8');
+        if (req.url === '/posts') {
+            getPost()
+            res.write('aaaa')
+        }
+        res.end()
+    })
+    .listen(3000)
 
-for(let i = a; i <= b; i++){
-    console.log(i)
-}
